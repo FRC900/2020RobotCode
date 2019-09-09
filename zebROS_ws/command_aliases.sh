6fa4b778bd306ba7df4ae0d4a34c66d1e55a9190
Caleb Krellwitz <cwkrellwitz@gmail.com>
2019-09-09 22:25:51 +0000

    Add various bash aliases for common commands

---
+++ zebROS_ws/command_aliases.sh
@@ -0,0 +1,8 @@
+# Aliases are short commands to replace long ones.
+
+alias build='~/2019Offseason/zebROS_ws/native_build.sh'
+alias crossbuild='~/2019Offseason/zebROS_ws/cross_build.sh'
+alias deploy='~/2019Offseason/deploy.sh'
+alias launch='roslaunch controller_node 2019_compbot_combined.launch'
+alias rosstd='source ~/2019Offseason/zebROS_ws/ROSStandard.sh'
+alias simlaunch='roslaunch controller_node 2019_compbot_combined.launch hw_or_sim:=sim joy_or_key:=key'
