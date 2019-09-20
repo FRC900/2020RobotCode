de17b98eb42db132c3d4d1ba8b456410ba7646e7
Caleb Krellwitz <cwkrellwitz@gmail.com>
2019-09-20 03:00:26 +0000

    Change "launch" to "hwlaunch"

--- zebROS_ws/command_aliases.sh
+++ zebROS_ws/command_aliases.sh
@@ -3,6 +3,6 @@
 alias build='~/2019Offseason/zebROS_ws/native_build.sh'
 alias crossbuild='~/2019Offseason/zebROS_ws/cross_build.sh'
 alias deploy='~/2019Offseason/deploy.sh'
-alias launch='roslaunch controller_node 2019_compbot_combined.launch'
+alias hwlaunch='roslaunch controller_node 2019_compbot_combined.launch'
 alias rosstd='source ~/2019Offseason/zebROS_ws/ROSStandard.sh'
 alias simlaunch='roslaunch controller_node 2019_compbot_combined.launch hw_or_sim:=sim joy_or_key:=key'
