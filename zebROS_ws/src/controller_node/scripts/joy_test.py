e5e3ee7340671d57b39d68d7a69aa8cf0122b12b
Caleb Krellwitz <cwkrellwitz@gmail.com>
2019-09-30 16:31:24 +0000

    Debug and specify joystick in startup print.

--- zebROS_ws/src/controller_node/scripts/joy_test.py
+++ zebROS_ws/src/controller_node/scripts/joy_test.py
@@ -11,16 +11,16 @@ def x_joystick_plugged():
     @return: True if joystick's mountpoint found.
     @rtype: bool
     """
-    PATH_JOYSTICK = "sys.argv[1]"
+    PATH_JOYSTICK = sys.argv[1]
     # https://stackoverflow.com/a/39124749/577001
     mount_points = {el.mountpoint: el for el in psutil.disk_partitions(all=True)}
     try:
         partition_joystick = mount_points[PATH_JOYSTICK][0]
-        print("Joystick found")
+        print("Joystick found at {}".format(PATH_JOYSTICK))
     except KeyError as e:
         syslog.syslog("x joystick's mount point {} not found.".format(PATH_JOYSTICK))
         syslog.syslog(str(e))
-        print("Joystick not found")
+        print("Joystick not found at {}".format(PATH_JOYSTICK))
 
 if __name__ == "__main__":
     x_joystick_plugged()
