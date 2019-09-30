831d0dea198363ee367acdfb15bad4f65547889f
Caleb Krellwitz <cwkrellwitz@gmail.com>
2019-09-30 22:21:30 +0000

    Turns out ls works better than a python script

--- zebROS_ws/joy_launch.sh
+++ zebROS_ws/joy_launch.sh
@@ -1,17 +1,17 @@
 # Would actually go at the bottom of .bashrc
 # Runs script to detect joysticks
 
-python ~/2019Offseason/zebROS_ws/src/controller_node/scripts/joy_test.py "/dev/input/js0"
+output=$(ls /dev/input/js0)
 
-if [ "$result" == "Joystick found at /dev/input/js0" ]; then
+if [ "$output" == "/dev/input/js0" ]; then
     export JOYSTICK_ZERO="true"
 else
     export JOYSTICK_ZERO="false"
 fi
 
-python ~/2019Offseason/zebROS_ws/src/controller_node/scripts/joy_test.py "/dev/input/js1"
+output=$(ls /dev/input/js0)
 
-if [ "$result" == "Joystick found at /dev/input/js1" ]; then
+if [ "$output" == "/dev/input/js1" ]; then
     export JOYSTICK_ONE="true"
 else
     export JOYSTICK_ONE="false"
