a82ee9091d95b1d4d12c71314c290768a65076c7
Kevin Jaget <kjaget@gmail.com>
2019-11-08 09:29:33 -0500

    Use uname -r to id module path rather than hard-coding

--- jetson_setup/can_up.sh
+++ jetson_setup/can_up.sh
@@ -18,7 +18,7 @@ do
 	sleep 1
 	rmmod gs_usb >> $log
 	sleep 1
-	insmod /lib/modules/4.9.140-tegra/kernel/drivers/net/can/usb/gs_usb.ko >> $log
+	insmod /lib/modules/`uname -r`/kernel/drivers/net/can/usb/gs_usb.ko >> $log
 done
 
 echo "Can0 successfully loaded" >> $log
