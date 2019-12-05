7e96ed5a4ed681c9aa8bb116525e321f52037ec4
Arvin S. Kushwaha <kushwaha21a@ncssm.edu>
2019-12-05 23:44:20 +0000

    Update

--- zebROS_ws/src/arvision/src/arvision_node.cpp
+++ zebROS_ws/src/arvision/src/arvision_node.cpp
@@ -1,25 +1,41 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
 
 using namespace cv;
 
 int main(int argc, char** argv)
 {
+    ros::init(argc, argv, "video_stream_node");
+    ros::NodeHandle nh;
+    image_transport::ImageTransport it(nh);
+    image_transport::Publisher pub = it.advertise("camera/image", 1);
+    ros::Rate loop_rate(5);
+
+    sensor_msgs::ImagePtr msg;
+    std_msgs::Header header;
+
     VideoCapture cap;
     if(!cap.open(0))
     {
         ROS_INFO("CANNOT OPEN VIDEOSTREAM");
         return 0;
     }
-    for(;;)
-    {
-        Mat frame;
-        cap >> frame;
-        if ( frame.empty() ) break;
-        imshow("this is you, smile", 255 - frame);
-        if( waitKey(10) == 27) break;
 
+    Mat frame;
+    while(ros::ok)
+    {
+        cap >> frame;
+        if (!frame.empty())
+        {
+            msg = cv_bridge::CvImage(header, "bgr8", frame).toImageMsg();
+            pub.publish(msg);
+            waitKey(1);
+        }
+        ros::spinOnce();
+        loop_rate.sleep();
     }
     return 0;
 }
