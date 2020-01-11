bb5852ad03f269ac0e67daacc2ee80f3d33fb4ca
Gabe Hart <flyingmessier@gmail.com>
2020-01-11 13:59:44 -0500

    added class structure, implemeted some methods

---
+++ zebROS_ws/src/pf_localization/src/particle.hpp
@@ -0,0 +1,13 @@
+#ifndef PARTICLE_HEADER
+#define PARTICLE_HEADER
+
+struct Particle {
+  double weight;
+  double x;
+  double y;
+  double rot;
+
+  Particle(double x, double y, double rot): x(x), y(y), rot(rot), weight(0) {}
+};
+
+#endif
--- zebROS_ws/src/pf_localization/src/world_model.hpp
+++ zebROS_ws/src/pf_localization/src/world_model.hpp
@@ -1,15 +1,22 @@
 #ifndef WORLD_MODEL_HEADER
 #define WORLD_MODEL_HEADER
 
+#include "particle.hpp"
+#include "hungarian.hpp"
+
 class WorldModel {
 private:
   std::vector<std::pair<double, double> > beacons_;
   double boundaries_[4]; // xmin, xmax, ymin, ymax
+  AssignmentProblemSolver<double> solver_;
 
 public:
   WorldModel();
   bool is_in_world(const Particle& p) const;
   void init_particle(Particle& p, double x, double y, double stdev);
+  double total_distance(const Particle& p, const std::vector<std::pair<double, double> >& m);
+  std::vector<double> distances(const std::pair<double, double> m) const;
+  std::vector<std::pair<double, double> > particle_relative(const Particle& p) const;
 };
 
 #endif
