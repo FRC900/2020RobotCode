d2543bdc44259754d7be2babbee076efcebc269f
Gabe Hart <flyingmessier@gmail.com>
2020-01-11 19:37:26 -0500

    finished preliminary implementation of algorithm

--- zebROS_ws/src/pf_localization/src/particle_filter.cpp
+++ zebROS_ws/src/pf_localization/src/particle_filter.cpp
@@ -5,9 +5,33 @@
 #include <utility>
 
 
-void ParticleFilter::init(double x, double y) {
+ParticleFilter::ParticleFilter(WorldModel w,
+                               double x_min, double x_max, double y_min, double y_max,
+                               size_t n) : world_(w), num_particles_(n) {
+  init(x_min, x_max, y_min, y_max);
+}
+
+void ParticleFilter::constrain_particles() {
   for (Particle p : particles_) {
-    world_.init_particle(p, x, y, init_stdev);
+    world_.constrain_to_world(p);
+  }
+}
+
+void ParticleFilter::init(double x_min, double x_max, double y_min, double y_max) {
+  particles_.reserve(num_particles_);
+  std::vector<double> bounds = world_.get_boundaries();
+  double x_l = std::max(x_min, bounds[0]);
+  double x_u = std::min(x_max, bounds[1]);
+  double y_l = std::max(y_min, bounds[2]);
+  double y_u = std::min(y_min, bounds[3]);
+  for (size_t i = 0; i < num_particles_; i++) {
+    std::uniform_real_distribution<double> x_distribution(x_l, x_u);
+  	std::uniform_real_distribution<double> y_distribution(y_l, y_u);
+    double x = x_distribution(rng_);
+    double y = y_distribution(rng_);
+    double rot = 0; //fix
+    Particle p = {x, y, rot};
+    particles_.push_back(p);
   }
 }
 
@@ -35,9 +59,9 @@ void ParticleFilter::resample() {
   std::vector<Particle> new_particles;
   new_particles.reserve(num_particles_);
   for (int i = 0; i < num_particles_; i++) {
-    double r = (rng_() - rng_.min()) / (rng_.max() - rng.min());
+    double r = (rng_() - rng_.min()) / (rng_.max() - rng_.min());
     double a = 0;
-    for (Particle p : particles) {
+    for (Particle p : particles_) {
       a += p.weight;
       if (a > r) {
         new_particles.push_back(p);
@@ -48,18 +72,28 @@ void ParticleFilter::resample() {
   particles_ = new_particles;
 }
 
-ParticleFilter(WorldModel w, double x, double y, int n);
-
 Particle ParticleFilter::predict() {
   return Particle(0, 0, 0); // TODO
 }
 
-void motion_update() {
-  // TODO
+void ParticleFilter::motion_update(double delta_x, double delta_y, double delta_rot) {
+  for (Particle p : particles_) {
+    p.x += delta_x;
+    p.y += delta_y;
+    p.rot += delta_rot;
+  }
+  noise();
+  constrain_particles();
 }
 
-void assign_weights(std::vector<std::pair<double, double> > mBeacons) {
-  for (Particle p : particles) {
+void ParticleFilter::set_rotation(double rot) {
+  for (Particle p : particles_) {
+    p.rot = rot;
+  }
+}
+
+void ParticleFilter::assign_weights(std::vector<std::pair<double, double> > mBeacons) {
+  for (Particle p : particles_) {
     p.weight = world_.total_distance(p, mBeacons);
   }
   normalize();
