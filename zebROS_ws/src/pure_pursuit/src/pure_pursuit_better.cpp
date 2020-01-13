854ec9c6364b93dc1279c661fb8c608ebbcafe6d
o-fugi <o.fugikawa@gmail.com>
2020-01-13 22:05:54 +0000

    added debug statements, fixed a couple of errors

--- zebROS_ws/src/pure_pursuit/src/pure_pursuit_better.cpp
+++ zebROS_ws/src/pure_pursuit/src/pure_pursuit_better.cpp
@@ -52,6 +52,7 @@ geometry_msgs::Pose PurePursuit::run(nav_msgs::Odometry odom, double &total_dist
     double dy;
 
     double magnitude_projection;
+    double distance_to_travel;
 
     // Find point in path closest to odometry reading
     // TODO (KCJ) - another possibility here is looking to see which segment between two wayponints
@@ -97,23 +98,24 @@ geometry_msgs::Pose PurePursuit::run(nav_msgs::Odometry odom, double &total_dist
             {
                 minimum_distance = distance_from_path;
                 current_waypoint_index = i;
+                ROS_INFO_STREAM("dx, dy = " << dx << ", " << dy << "; magnitude_projection = " << magnitude_projection);
+                distance_to_travel = hypot(dx, dy) - magnitude_projection; // Current value is distance that the robot will travel to reach the next waypoint
             }
         }
     }
 
     if(minimum_distance == std::numeric_limits<double>::max())
     {
-        ROS_ERROR_STREAM("Not in path");
+        ROS_ERROR_STREAM("Not within path limits"); //TODO: drive toward beginning of path?
     }
-
 
     ROS_INFO_STREAM("minimum_distance = " << minimum_distance);
 
-    double distance_to_travel = hypot(dx, dy) - magnitude_projection; // Current value is distance that the robot will travel to reach the next waypoint
     // Determine next waypoint
     size_t end_i = current_waypoint_index;
     for(; end_i < num_waypoints_ - 1; ++end_i)
     {
+        ROS_INFO_STREAM("index = " << end_i << ", distance_to_travel = " << distance_to_travel);
         start_x = path_.poses[end_i].pose.position.x;
         start_y = path_.poses[end_i].pose.position.y;
         end_x = path_.poses[end_i+1].pose.position.x;
@@ -130,6 +132,7 @@ geometry_msgs::Pose PurePursuit::run(nav_msgs::Odometry odom, double &total_dist
     dy = end_y - start_y;
     double final_x = path_.poses[end_i].pose.position.x + (lookahead_distance_ - distance_to_travel) * (dx / hypot(dx, dy)); 
     double final_y = path_.poses[end_i].pose.position.y + (lookahead_distance_ - distance_to_travel) * (dy / hypot(dx, dy)); 
+    ROS_INFO_STREAM("drive to coordinates: (" << final_x << ", " << final_y << ")");
 
     // Determine target orientation
     double roll, pitch, start_yaw, end_yaw;
@@ -148,6 +151,7 @@ geometry_msgs::Pose PurePursuit::run(nav_msgs::Odometry odom, double &total_dist
     tf::Matrix3x3(waypoint_q_end).getRPY(roll, pitch, end_yaw);
     // Find orientation between the waypoints
     double final_orientation = start_yaw + (end_yaw - start_yaw) * ((lookahead_distance_ - distance_to_travel) / hypot(dx, dy));
+    ROS_INFO_STREAM("drive to orientation: " << final_orientation);
     // Convert back to quaternion
     geometry_msgs::Quaternion q_final = tf::createQuaternionMsgFromRollPitchYaw(0.0, 0.0, final_orientation);
 
