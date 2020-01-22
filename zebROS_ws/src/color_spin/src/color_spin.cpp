b2a7ab3df40a2c23fb05b5d3d0dc4a1519694056
JoahanC <joahan.castanedajaimes@gmail.com>
2020-01-22 23:09:33 +0000

    removed unnecessary file and modified cpp file

--- zebROS_ws/src/color_spin/src/color_spin.cpp
+++ zebROS_ws/src/color_spin/src/color_spin.cpp
@@ -1,51 +1,51 @@
 #include "ros/ros.h"
 #include "color_spin/color_algorithm.h"
 
-color_spin::color_algorithm::Request &req,
-color_spin::color_algorithm::Response &res
+bool add(color_spin::color_algorithm::Request &req,
+		color_spin::color_algorithm::Response &res)
 {
-  if (sensor_color == 'r') {
-	  if (fms_color == 'y') {
-		  rotate = .125;
+  if (req.sensor_color == 'r') {
+	  if (req.fms_color == 'y') {
+		  res.rotate = .125;
 		}
-	  else if (fms_color == 'c') {
-		  rotate = .25;
+	  else if (req.fms_color == 'c') {
+		  res.rotate = .25;
 		}
-	  else if (fms_color == 'g') {
-		  rotate = -.125;
-		}
-	  }
-  else if (sensor_color == 'y') {
-	  if (fms_color == 'r') {
-		  rotate = -.125;
-		}
-	  else if (fms_color == 'c') {
-		  rotate = .125;
-		}
-	  else if (fms_color == 'g') {
-		  rotate = .25;
+	  else if (req.fms_color == 'g') {
+		  res.rotate = -.125;
 		}
 	  }
-  else if (sensor_color == 'c') {
-	  if (fms_color == 'r') {
-		  rotate = .25;
+  else if (req.sensor_color == 'y') {
+	  if (req.fms_color == 'r') {
+		  res.rotate = -.125;
 		}
-	  else if (fms_color == 'y') {
-		  rotate = -.125;
+	  else if (req.fms_color == 'c') {
+		  res.rotate = .125;
 		}
-	  else if (fms_color == 'g') {
-		  rotate = -.125;
+	  else if (req.fms_color == 'g') {
+		  res.rotate = .25;
 		}
 	  }
-  else if (sensor_color == 'g') {
-	  if (fms_color == 'r') {
-		  rotate = .125;
+  else if (req.sensor_color == 'c') {
+	  if (req.fms_color == 'r') {
+		  res.rotate = .25;
 		}
-	  else if (fms_color == 'y') {
-		  rotate = .25;
+	  else if (req.fms_color == 'y') {
+		  res.rotate = -.125;
 		}
-	  else if (fms_color == 'c') {
-		  rotate = -.125
+	  else if (req.fms_color == 'g') {
+		  res.rotate = -.125;
+		}
+	  }
+  else if (req.sensor_color == 'g') {
+	  if (req.fms_color == 'r') {
+		  res.rotate = .125;
+		}
+	  else if (req.fms_color == 'y') {
+		  res.rotate = .25;
+		}
+	  else if (req.fms_color == 'c') {
+		  res.rotate = -.125
   ROS_INFO("request = x%1d, y=%1d", (char)req.fms_color, (char)req.sensor_color.b);
   ROS_INFO("sending response = [x%1d]", (float)res.rotate);
   return true;
