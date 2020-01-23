c7e568879a3e9adeb3b056cb1705a32bf9053488
Adam <adamkosinski631@gmail.com>
2020-01-23 02:56:52 +0000

    More work on developing auto node, figured out how reading config is going to work

--- zebROS_ws/src/behaviors/src/auto_node.cpp
+++ zebROS_ws/src/behaviors/src/auto_node.cpp
@@ -1,12 +1,15 @@
 #include "ros/ros.h"
-#include "behavior_actions/AutoMode.h" //TODO dependencies for this
-
+#include "behavior_actions/AutoMode.h"
 
 //variables to store stuff read from config
 int auto_mode;
-std::vector</*string*/> auto_step_names;
-std::vector</*something*/> auto_step;
+std::vector<std::string> auto_steps; //stores string of action names to do, read from the auto mode array in the config file
 
+bool auto_running = false;
+//set to true when enter auto mode (not set false when enter teleop b/c we'll let this node keep running)
+//set false if something fails
+//set false if driver overrides auto continuing (during teleop)
+//Everything checks if this is true before proceeding.
 
 
 int main(int argc, char** argv)
@@ -25,20 +28,15 @@ int main(int argc, char** argv)
 	//actionlib clients
 	//TODO
 
+
 	//other variables
-	int auto_mode = -1; //-1 indicates that no auto mode has been picked - don't execute anything //TODO reevaluate this
-
-	bool auto_running = false;
-	//set to true when enter auto mode (not set false when enter teleop b/c we'll let this node keep running)
-	//set false if something fails
-	//set false if driver overrides auto continuing (during teleop)
-	//Everything checks if this is true before proceeding.
-
 	ros::Rate r(10); //TODO: what rate?
 
 
 	//WAIT FOR MATCH TO START --------------------------------------------------------------------------
+	ROS_INFO("Auto node - waiting for autonomous to start");
 
+	auto_running = true; //TODO remove
 	//wait for auto period to start
 	while(!auto_running)
 	{
@@ -57,16 +55,42 @@ int main(int argc, char** argv)
 
 	//EXECUTE AUTONOMOUS ACTIONS --------------------------------------------------------------------------
 
+	//read sequence of actions
+	ROS_INFO("Auto node - reading config values based on the selected auto mode");
+	nh.getParam("auto_mode_1", auto_steps); //TODO if it failed
+
+
 	//run through actions in order
-	for(){
+	for(size_t i = 0; i < auto_steps.size(); i++){
 		if(auto_running)
 		{
+			ROS_INFO_STREAM("Auto node - running step " << i << ": " << auto_steps[i]);
 
+			//read data needed to carry out the action
+			XmlRpc::XmlRpcValue action_data;
+			nh.getParam(auto_steps[i], action_data);
+
+			//figure out what to do based on the action type
+			const char* action_type = action_data["type"];
+			switch(action_type) //TODO can't do switch like this
+			{
+				case "intake_actionlib_server":
+					ROS_INFO("intake actionlib server");
+					break;
+				default:
+					ROS_INFO_STREAM("Invalid type of action: " << action_type);
+			}
 		}
 
 	}
 
 }
+
+
+
+
+
+
 
 
 
@@ -78,24 +102,9 @@ int main(int argc, char** argv)
 //subscriber callback for dashboard data
 void autoModeCallback(const behavior_actions::AutoMode::ConstPtr& msg)
 {
-	//read msg data
-	//TODO do it
-
-	//only change auto mode and reread config
-	if(/*new auto mode*/ != auto_mode){
-		auto_mode = /*new auto mode*/
-	}
-	else {
-		return;
-	}
-
-
-	//if no auto mode selected, don't do anything
-	if(auto_mode == -1)
+	//only change auto mode if auto hasn't started
+	if(!auto_running)
 	{
-		return;
+		//TODO read data
 	}
-
-	//read details of the auto sequence
-	//TODO probably with the XmlRpc method??
 }
