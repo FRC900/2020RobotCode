da277d898d9b5ed7b47e2a38de8e9f5446f531a4
JoahanC <joahan.castanedajaimes@gmail.com>
2020-01-23 00:49:58 +0000

    fixed semicolon positioning

--- zebROS_ws/src/color_spin/src/color_spin.cpp
+++ zebROS_ws/src/color_spin/src/color_spin.cpp
@@ -1,7 +1,7 @@
 #include "ros/ros.h"
 #include "color_spin/color_algorithm.h"
 
-bool add(color_spin::color_algorithm::Request &req,
+bool rotate(color_spin::color_algorithm::Request &req,
 		color_spin::color_algorithm::Response &res)
 {
   if (req.sensor_color == 'r') {
@@ -45,18 +45,19 @@ bool add(color_spin::color_algorithm::Request &req,
 		  res.rotate = .25;
 		}
 	  else if (req.fms_color == 'c') {
-		  res.rotate = -.125 
+		  res.rotate = -.125;
 		}
-  ROS_INFO("request = x%1d, y=%1d", (char)req.fms_color, (char)req.sensor_color.b);
-  ROS_INFO("sending response = [x%1d]", (float)res.rotate);
+  }
+  ROS_INFO("request = x%1c, y=%1c", (char)req.fms_color, (char)req.sensor_color);
+  ROS_INFO("sending response = [x%4d]", (float)res.rotate);
   return true;
 }
 
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "color_spin_rotation");
+	ros::init(argc, argv, "color_spin_algorithm");
 	ros::NodeHandle nh;
-	ros::ServiceServer service = n.advertiseService("color_spin_rotation", rotate);
+	ros::ServiceServer service = nh.advertiseService("color_spin_algorithm", rotate);
 	ROS_INFO("Ready to calculate rotation");
 	ros::spin();
 
