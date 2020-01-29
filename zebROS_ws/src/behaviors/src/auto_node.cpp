ff8ebc9d455347982b989f71f08c648ab370e536
Adam <adamkosinski631@gmail.com>
2020-01-29 05:46:48 +0000

    Added wait for actionlib server function, added pause action

--- zebROS_ws/src/behaviors/src/auto_node.cpp
+++ zebROS_ws/src/behaviors/src/auto_node.cpp
@@ -14,8 +14,8 @@
 int auto_mode = -1; //-1 if nothing selected
 std::vector<std::string> auto_steps; //stores string of action names to do, read from the auto mode array in the config file
 
-bool auto_started = false; //set to true when enter auto time period (not set false when enter teleop b/c we'll let this node keep running)
-bool auto_stopped = false; //set to true if driver stops auto (see the service, calls stopAuto() )
+bool auto_started = false; //set to true when enter auto time period
+bool auto_stopped = false; //set to true if driver stops auto (callback: stopAuto() ) - note: this node will keep doing actions during teleop if not finished and the driver doesn't stop auto
 //All actions check if(auto_started && !auto_stopped) before proceeding.
 
 std::atomic<int> auto_state(0); //This state is published by the publish thread
@@ -23,20 +23,16 @@ std::atomic<int> auto_state(0); //This state is published by the publish thread
 
 
 //FUNCTIONS -------
-//function to wait while an actionlib server is running
-//TODO: steal from actionlib server template maybe
-
-
-
 
 //server callback for stop autonomous execution
 bool stopAuto(behavior_actions::StopAuto::Request &req,
 			  behavior_actions::StopAuto::Response &res)
 {
-	ROS_INFO("Stopping auto execution");
+	ROS_INFO("Auto node - Stopping code");
 	auto_stopped = true;
 	return true;
 }
+
 
 //subscriber callback for match data
 void matchDataCallback(const frc_msgs::MatchSpecificData::ConstPtr& msg)
@@ -48,13 +44,11 @@ void matchDataCallback(const frc_msgs::MatchSpecificData::ConstPtr& msg)
 }
 
 
-
 //subscriber callback for dashboard data
 void updateAutoMode(const behavior_actions::AutoMode::ConstPtr& msg)
 {
 	auto_mode = msg->auto_mode;
 }
-
 
 
 
@@ -79,6 +73,48 @@ void publishAutoState(ros::Publisher publisher)
 }
 
 
+
+//function to wait while an actionlib server is running
+void waitForActionlibServer(auto &action_client, double timeout, const std::string &activity)
+	//activity is a description of what we're waiting for, e.g. "waiting for mechanism to extend" - helps identify where in the server this was called (for error msgs)
+{
+	double request_time = ros::Time::now().toSec();
+	ros::Rate r(10); //TODO config?
+
+	//wait for actionlib server to finish
+	std::string state;
+	while(!auto_stopped && ros::ok())
+	{
+		state = action_client.getState().toString();
+
+		if(state == "PREEMPTED") {
+			ROS_ERROR_STREAM("Auto node - " << activity << " got preempted");
+			auto_stopped = true;
+		}
+		//check timeout - note: have to do this before checking if state is SUCCEEDED since timeouts are reported as SUCCEEDED
+		else if (ros::Time::now().toSec() - request_time > timeout || //timeout from what this file says
+				(state == "SUCCEEDED" && !action_client.getResult()->success)) //server times out by itself
+		{
+			ROS_ERROR_STREAM("Auto node - " << activity << " timed out");
+			auto_stopped = true;
+			action_client.cancelGoalsAtAndBeforeTime(ros::Time::now());
+		}
+		else if (state == "SUCCEEDED") { //must have succeeded since we already checked timeout possibility
+			break; //stop waiting
+		}
+		else if (auto_stopped){
+			action_client.cancelGoalsAtAndBeforeTime(ros::Time::now());
+		}
+		else { //if didn't succeed and nothing went wrong, keep waiting
+			ros::spinOnce();
+			r.sleep();
+		}
+	}
+}
+
+
+
+
 int main(int argc, char** argv)
 {
 	//SETUP --------------------------------------------------------------------------------------------
@@ -91,7 +127,7 @@ int main(int argc, char** argv)
 	//rio match data (to know if we're in auto period)
 	ros::Subscriber match_data_sub = nh.subscribe("/frcrobot_rio/match_data", 1, matchDataCallback);
 	//dashboard (to get auto mode)
-	ros::Subscriber auto_mode_sub = nh.subscribe("auto_mode", 1, updateAutoMode); //TODO get correct topic name
+	ros::Subscriber auto_mode_sub = nh.subscribe("auto_mode", 1, updateAutoMode); //TODO get correct topic name (namespace)
 
 	//publishers
 	//auto state
@@ -108,17 +144,16 @@ int main(int argc, char** argv)
 
 
 	//other variables
-	ros::Rate r(10); //TODO: what rate?
+	ros::Rate r(10); //used in various places where we wait TODO: config?
 
 
 	//WAIT FOR MATCH TO START --------------------------------------------------------------------------
 	ROS_INFO("Auto node - waiting for autonomous to start");
 
 	//wait for auto period to start
-	while( (!auto_started && !auto_stopped) || auto_mode <= 0) //last one checks if we selected an auto mode yet
+	while( (!auto_started || auto_mode <= 0) && !auto_stopped ) //the auto_mode check is for if we selected an auto mode yet
 	{
 		if(!ros::ok()){
-			ROS_ERROR("Killing auto node b/c ros not ok when waiting for autonomous to start");
 			auto_state = 3; //done
 			auto_state_pub_thread.join(); //keeps the publishing going then closes the publish thread when ros not ok
 			return 0;
@@ -130,14 +165,14 @@ int main(int argc, char** argv)
 			auto_state = 1; //ready
 		}
 		if(auto_started && auto_mode <= 0){
-			ROS_ERROR("Please choose an auto mode");
+			ROS_ERROR("Auto node - Autonomous period started, please choose an auto mode");
 		}
 
 		r.sleep();
 	}
 
 	if(auto_stopped){
-		ROS_INFO("Auto stopped before execution");
+		ROS_INFO("Auto node - code stopped before execution");
 		auto_state = 3; //done
 		auto_state_pub_thread.join();
 		return 0;
@@ -172,16 +207,40 @@ int main(int argc, char** argv)
 				return 1;
 			}
 
+
+			// CODE FOR ACTIONS HERE --------------------------------------------------
 			//figure out what to do based on the action type, and do it
-			if(action_data["type"] == "intake_actionlib_server")
+
+			if(action_data["type"] == "pause")
 			{
-				//do stuff
-				ros::Duration(3).sleep();
+				const double start_time = ros::Time::now().toSec();
+
+				//read duration - user could've entered a double or an int, we don't know which
+				double duration;
+				if(action_data["duration"].getType() == XmlRpc::XmlRpcValue::Type::TypeDouble) {
+					duration = (double) action_data["duration"];
+				}
+				else if(action_data["duration"].getType() == XmlRpc::XmlRpcValue::Type::TypeInt) {
+					duration = (double) (int) action_data["duration"];
+				}
+				else {
+					ROS_ERROR_STREAM("Auto node - duration is not a double or int in '" << auto_steps[i] << "' action");
+					auto_state = -1;
+					auto_state_pub_thread.join();
+					return 1;
+				}
+
+				//wait
+				while (ros::Time::now().toSec() - start_time < duration && !auto_stopped && ros::ok())
+				{
+					ros::spinOnce();
+					r.sleep();
+				}
 			}
-			else if(action_data["type"] == "pause")
+			//placeholder - TODO actually do stuff here
+			else if(action_data["type"] == "intake_actionlib_server")
 			{
 				//do stuff
-				ros::Duration(3).sleep();
 			}
 			//TODO remove test
 			else if(action_data["type"] == "elevator_actionlib_server")
@@ -190,10 +249,11 @@ int main(int argc, char** argv)
 				behavior_actions::ElevatorGoal goal;
 				goal.setpoint_index = (int) action_data["goal"]["setpoint_index"];
 				elevator_ac.sendGoal(goal);
+				waitForActionlibServer(elevator_ac, 100, "elevator server thing");
 			}
 			else
 			{
-				ROS_ERROR_STREAM("Invalid type of action: " << action_data["type"]);
+				ROS_ERROR_STREAM("Auto node - Invalid type of action: " << action_data["type"]);
 				auto_state = -1;
 				auto_state_pub_thread.join(); //keeps the publishing going then closes the publish thread when ros not ok
 				return 1;
@@ -203,10 +263,10 @@ int main(int argc, char** argv)
 	}
 
 	if(auto_stopped){
-		ROS_INFO("Autonomous actions stopped before completion");
+		ROS_INFO("Auto node - Autonomous actions stopped before completion");
 	}
 	else {
-		ROS_INFO("Autonomous actions completed!");
+		ROS_INFO("Auto node - Autonomous actions completed!");
 	}
 	auto_state = 3; //done
 	auto_state_pub_thread.join(); //keeps the publishing going then closes the publish thread when ros not ok
