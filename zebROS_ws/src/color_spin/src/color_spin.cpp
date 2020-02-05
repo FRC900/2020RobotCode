de0fe0a7c02d45bea25289dac3a4dd4a9ddfdd72
Nathan <nathan.s.yang@gmail.com>
2020-02-05 23:26:36 +0000

    Fixed issues with variable typing

--- zebROS_ws/src/color_spin/src/color_spin.cpp
+++ zebROS_ws/src/color_spin/src/color_spin.cpp
@@ -1,54 +1,55 @@
 #include "ros/ros.h"
+#include "string"
 #include "color_spin/color_algorithm.h"
 
 bool rotate(color_spin::color_algorithm::Request &req,
 		color_spin::color_algorithm::Response &res)
 {
-  if (req.sensor_color == 'r') {
-	  if (req.fms_color == 'y') {
+  if (req.sensor_color == "r") {
+	  if (req.fms_color == "y") {
 		  res.rotate = .125;
 		}
-	  else if (req.fms_color == 'c') {
+	  else if (req.fms_color == "c") {
 		  res.rotate = .25;
 		}
-	  else if (req.fms_color == 'g') {
+	  else if (req.fms_color == "g") {
 		  res.rotate = -.125;
 		}
 	  }
-  else if (req.sensor_color == 'y') {
-	  if (req.fms_color == 'r') {
+  else if (req.sensor_color == "y") {
+	  if (req.fms_color == "r") {
 		  res.rotate = -.125;
 		}
-	  else if (req.fms_color == 'c') {
+	  else if (req.fms_color == "c") {
 		  res.rotate = .125;
 		}
-	  else if (req.fms_color == 'g') {
+	  else if (req.fms_color == "g") {
 		  res.rotate = .25;
 		}
 	  }
-  else if (req.sensor_color == 'c') {
-	  if (req.fms_color == 'r') {
+  else if (req.sensor_color == "c") {
+	  if (req.fms_color == "r") {
 		  res.rotate = .25;
 		}
-	  else if (req.fms_color == 'y') {
+	  else if (req.fms_color == "y") {
 		  res.rotate = -.125;
 		}
-	  else if (req.fms_color == 'g') {
+	  else if (req.fms_color == "g") {
 		  res.rotate = -.125;
 		}
 	  }
-  else if (req.sensor_color == 'g') {
-	  if (req.fms_color == 'r') {
+  else if (req.sensor_color == "g") {
+	  if (req.fms_color == "r") {
 		  res.rotate = .125;
 		}
-	  else if (req.fms_color == 'y') {
+	  else if (req.fms_color == "y") {
 		  res.rotate = .25;
 		}
-	  else if (req.fms_color == 'c') {
+	  else if (req.fms_color == "c") {
 		  res.rotate = -.125;
 		}
 	  }
-  ROS_INFO("request = x%1c, y=%1c", (char)req.fms_color, (char)req.sensor_color);
+  ROS_INFO("request = x=%1s, y=%1s", req.fms_color.c_str(), req.sensor_color.c_str());
   ROS_INFO("sending response = [x%1f]", (float)res.rotate);
   return true;
 }
