0245b6cf3ddcfc3eb7f31d883417021e001726b5
ashlyn <ashlynjoshy@gmail.com>
2020-02-11 23:12:56 +0000

    added more dimensions

--- zebROS_ws/src/tensorflow_class/src/tensorflow_class.cpp
+++ zebROS_ws/src/tensorflow_class/src/tensorflow_class.cpp
@@ -14,23 +14,84 @@ public:
 	{
 		if (object_type == "power_cell")
 		{
-			height_ = .178 
-			width_ = .178
-			depth_ = .178
+			height_ = .178;
+			width_ = .178;
+			depth_ = .178;
 		}
 
 		else if (object_type == "red_power_port_high_goal" or "blue_power_port_high_goal")
 		{
-			height_ = 0.76
-			width_ = 0.76
-			depth_ = 0
+			height_ = 0.76;
+			width_ = 0.76;
+			depth_ = 0;
 		}
 
-		else if (object_type == "red_loading_bay
-		
+		else if (object_type == "red_power_port_low_goal" or "blue_power_port_low_goal")
+		{
+			height_ = 0.254;
+			width_ = 0.86;
+			depth_ = 0;
+		}
+
+		else if (object_type == "red_loading_bay_tape" or "blue_loading_bay_tape")
+		{
+			height_ = 0.533;
+			width_ = 0.62;
+			depth_ = 0;
+		}
+
+		else if (object_type == "red_loading_bay_left_graphics" or "blue_loading_bay_left_graphics")
+		{
+			height_ = 0.4318;
+			width_ = 0.1651;
+			depth_ = 0;
+		}
+
+		else if (object_type == "red_loading_bay_right_graphics" or "blue_loading_bay_right_graphics")
+		{
+			height_ = 0.654;
+			width_ = 0.1651;
+			depth_ = 0;
+		}
+
+		else if (object_type == "red_ds_light" or "blue_ds_light" or "ds_light" or "control_panel_light" or "yellow_control_panel_light" or "shield_generator_light" or "red_shield_generator_light" or "blue_shield_generator_light")
+		{
+			height_ = 0.214;
+			width_ = 0.0762;
+			depth_ = 0.0762;
+		}
+
+		else if (object_type == "shield_generator_backstop")
+		{
+			height_ = 2.912;
+			width_ = 2.157;
+			depth_ = 3.124;
+		}
+
+		else if (object_type == "shield_generator_first_logo")
+		{
+			height_ = 0.203;
+			width_ = 0.203;
+			depth_ = 0;
+		}
+
+		else if (object_type == "shield_generator_yellow_stripe")
+		{
+			height_ = 0.5334;
+			width_ = 0.508;
+			depth_ = 0;
+		}
+
+		else if (oject_type == "control_panel")
+		{
+			height_ = 0.05;
+			width_ = 0.81;
+			depth_ = 0.81;
+		}
 
 	}
 
+	
 	double getHeight()
 	{
 		return height_;
