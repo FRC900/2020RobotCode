903fffd60b6d523bf8b04a7b9c85be3c8605c769
Yashas Ambati <yashas.amb@gmail.com>
2020-02-15 17:02:27 -0500

    tensorflow stuff seems to be working with ros!

--- zebROS_ws/src/tf_object_detection/src/test_detection.py
+++ zebROS_ws/src/tf_object_detection/src/test_detection.py
@@ -141,7 +141,7 @@ def main():
 
     # Path to frozen detection graph. This is the actual model that is used for the object detection.
     # This shouldn't need to change
-    PATH_TO_FROZEN_GRAPH = os.path.join(THIS_DIR, 'frozen_inference_graph.pb')
+    PATH_TO_FROZEN_GRAPH = THIS_DIR + 'frozen_inference_graph.pb'
 
     # List of the strings that is used to add correct label for each box.
     PATH_TO_LABELS = os.path.join(THIS_DIR, '2020Game_label_map.pbtxt')
