ba001eb5268130b8df33d608e9f655af77619c18
Yashas Ambati <yashas.amb@gmail.com>
2020-02-19 20:23:29 -0500

    added time sync, getting errors

--- zebROS_ws/src/screen_to_world/src/screen_to_world.cpp
+++ zebROS_ws/src/screen_to_world/src/screen_to_world.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
-#include <ros/ros.h>
+#include <string>
 
+#include <ros/ros.h>
 #include <opencv2/opencv.hpp>
 #include <image_geometry/pinhole_camera_model.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/exact_time.h>
+#include <sensor_msgs/Image.h>
 
 #include <field_obj/Object.h>
 #include <field_obj/Detection.h>
+#include <field_obj/TFObject.h>
+#include <field_obj/TFDetection.h>
 
 ros::Publisher pub;
 image_geometry::PinholeCameraModel model_;
@@ -24,30 +31,46 @@ void camera_info_callback(const sensor_msgs::CameraInfoConstPtr &info) {
 	camera_info_ = *info;
 }
 
-void screen_to_world_callback(field_obj::Detection::ConstPtr& raw_msg){
+void screen_to_world_callback(const field_obj::TFDetection::ConstPtr &raw_msg, const sensor_msgs::ImageConstPtr &depth_msg){
   float distance;
   field_obj::Detection msg;
-  for(field_obj::Detection detection : raw_msg -> objects){
-    screen_x = detection.location.x;
-    screen_y = detection.location.y;
+  for(field_obj::TFObject object : raw_msg -> objects){
+    double screen_x = (object.tl.x + object.br.x) / 2;
+    double screen_y = (object.tl.y + object.br.y) / 2;
 
     cv::Point2f screen_coord(screen_x, screen_y);
-    cv::Point3f world_coord = convert_to_world(screen_coord, distance)
-    detection.location.x = world_coord.x;
-    detection.location.y = world_coord.y;
-    detection.location.z = world_coord.z;
+    cv::Point3f world_coord = convert_to_world(screen_coord, distance);
+
+    field_obj::Object dummy;
+
+    dummy.location.x = world_coord.x;
+    dummy.location.y = world_coord.y;
+    dummy.location.z = world_coord.z;
+    dummy.id = object.label;
+    dummy.confidence = object.confidence;
+
+    msg.objects.push_back(dummy);
   }
+  pub.publish(msg);
 }
 
-int main(int argc, char* argv) {
+int main(int argc, char* argv[]) {
   ros::init(argc, argv, "screen_to_world_node");
   ros::NodeHandle nh_;
+  std::string sub_topic, camera_info_topic, depth_topic, pub_topic;
 
   model_.fromCameraInfo(camera_info_);
 
-  pub = node.advertise<field_obj::Detection>(pub_topic, 1);
-  ros::Subscriber sub_screen = nh_.subscribe(sub_topic, 1, screen_to_world_callback);
-  ros::Subscriber sub_camera_info = nh_.subscribe("/zed_goal/left/camera_info", sub_rate, camera_info_callback);
+  pub = nh_.advertise<field_obj::Detection>(pub_topic, 1);
+  ros::Subscriber sub_camera_info = nh_.subscribe(camera_info_topic, 1, camera_info_callback);
+
+  message_filters::Subscriber<field_obj::TFDetection> tf_sub(nh_, sub_topic, 1);
+  message_filters::Subscriber<sensor_msgs::Image> depth_sub(nh_, depth_topic, 1);
+
+  typedef message_filters::sync_policies::ExactTime<sensor_msgs::Image, field_obj::TFDetection> MySyncPolicy;
+  // ExactTime takes a queue size as its constructor argument, hence MySyncPolicy(10)
+  message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), tf_sub, depth_sub);
+  sync.registerCallback(boost::bind(&screen_to_world_callback, _1, _2));
 
   ros::spin();
   return 0;
