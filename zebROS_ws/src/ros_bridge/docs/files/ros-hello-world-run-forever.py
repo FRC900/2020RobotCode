7c9283649668275bd92ca5186ad47d956bc2bf9a
Adam <adamkosinski631@gmail.com>
2020-03-10 18:33:12 -0400

    rosbridge package fix

--- zebROS_ws/src/ros_bridge/docs/files/ros-hello-world-run-forever.py
+++
@@ -1,6 +0,0 @@
-from __future__ import print_function
-import roslibpy
-
-client = roslibpy.Ros(host='localhost', port=9090)
-client.on_ready(lambda: print('Is ROS connected?', client.is_connected))
-client.run_forever()
